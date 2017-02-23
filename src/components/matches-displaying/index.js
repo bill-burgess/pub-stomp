3a4070f5dd922516b309f92e91006185786ab908
Bill Burgess <wmhburgess@gmail.com>
2017-02-24 10:56:12 +1300

    adds an api route to get all matches, wip

--- src/components/matches-displaying/index.js
+++ src/components/matches-displaying/index.js
@@ -1,30 +1,40 @@
 const React = require('react')
-const _ = require('lodash')
+const request = require('superagent')
 
 const filterMatches = require('./filter-matches')
 
-const MatchesDisplaying = (props) => {
-  const { matches, dispatch, router, filters } = props
+class MatchesDisplaying extends React.Component {
 
-  function displayMatches(matches, filters){
-    const displayingMatches = filterMatches(matches, filters)
-    return displayingMatches.map(match => {
-      const { tournament, id, team1, team2, date } = match
-      return (
-        <div key={id}>
-          <p>{tournament}</p>
-          <span>{team1.name} VS {team2.name}</span>
-          <p>{date}</p><br />
-        </div>
-      )
+  componentDidMount(){
+    request.get('api/v1/matches', (err, res) => {
+      if(err) console.error(err)
+      console.log('Got the things!', res)
     })
   }
 
-  return (
-    <div>
+  render(){
+    const { matches, dispatch, router, filters } = this.props
+
+    function displayMatches(matches, filters){
+      const displayingMatches = filterMatches(matches, filters)
+      return displayingMatches.map(match => {
+        const { tournament, id, team1, team2, date } = match
+        return (
+          <div key={id}>
+          <p>{tournament}</p>
+          <span>{team1.name} VS {team2.name}</span>
+          <p>{date}</p><br />
+          </div>
+        )
+      })
+    }
+
+    return (
+      <div>
       {displayMatches(matches, filters)}
-    </div>
-  )
+      </div>
+    )
+  }
 }
 
 module.exports = MatchesDisplaying
